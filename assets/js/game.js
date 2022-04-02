7e6aac8b78ca1a3705eaf29de69226118db7dd24
tevinalvord <tevin.alvord@gmail.com>
2022-04-02 00:14:46 -0600

    add player and enemy stat variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,31 @@
 var playerName = window.prompt("What is your robot's name?")
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+var enemyName = 'Roborto';
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log('This logs a string, good for leaving yourself a message');
 
-console.log (10 + 10);
+var fight = function() {
+    // Alert the players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-console.log("Our robot's name is " + playerName);
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
 
-function fight() {
-    window.alert("The fight has begun!");
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      );
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      );
 }
 
-// fight();
+fight();

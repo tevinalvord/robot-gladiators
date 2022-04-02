ddad9177f81a51d20370f847f7827b32019f6130
tevinalvord <tevin.alvord@gmail.com>
2022-04-02 17:07:32 -0600

    add enemy combatans

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,12 +3,11 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = 'Roborto';
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-
-var fight = function() {
+var fight = function(enemyName) {
     // Alert the players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -60,4 +59,7 @@ var fight = function() {
     }
 }
 
-fight();
+
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}

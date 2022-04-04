477c1a4fe056c7c78a0b5d535645dc069fa3ac6a
tevinalvord <tevin.alvord@gmail.com>
2022-04-03 21:37:50 -0600

    added highscore functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -124,13 +124,26 @@ var startGame = function() {
 };
 
 var endGame = function() {
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0){
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    } else {
-        window.alert("The game has now ended. Let's see how you did!")
-    }
+    window.alert("The game has now ended. Let's see how you did!")
     
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highScore");
+
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highScore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        window.alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } else {
+        window.alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+
+    // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
